1169b553ecd90ff5bf92dfd0e5280a098deaedaf
Turaska-Chandra-Barman <barmanjatin372@gmail.com>
2024-07-04 16:17:29 +0600

    fsd

--- index.js
+++
@@ -1,7 +0,0 @@
-const html = document.getElementById('darkHtml');
-const btn = document.getElementById('switch');
-
-console.log(btn);
-btn.addEventListener('click',function(e){
-    html.classList.toggle('dark')
-})
